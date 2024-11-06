2e4f4af031dc77ef7c90b21b3f84273dff0f9d36
Tim MacKay <simplytim42@gmail.com>
2024-11-06 10:14:37 +0000

    add publish endpoint

--- sak/main.py
+++ sak/main.py
@@ -1,15 +1,22 @@
 import typer
 import pyperclip
 import json
+import validators
 from pathlib import Path
 from openai import OpenAI
 from rich import print
 from typing_extensions import Annotated
+from .blog_parser import BlogPostParser
 
 from .helper import POST_REVIEWER_CONTENT, DESCRIPTION_GENERATOR_CONTENT, EXCERPT_GENERATOR_CONTENT
 
+overview = """
+Swiss Army Knife (sak).
 
-app = typer.Typer(no_args_is_help=True)
+The following env vars need to exist: OPENAI_API_KEY, MEDIUM_API_KEY
+"""
+
+app = typer.Typer(no_args_is_help=True, help=overview)
 
 
 @app.command()
@@ -102,3 +109,44 @@ def introduce(
     selection = int(typer.prompt("Which except would you like to copy?"))
 
     pyperclip.copy(exceprts[selection - 1])
+
+
+@app.command()
+def publish(
+        blog_filepath: Annotated[
+            Path, typer.Argument(help="The local filepath of the markdown file of the blog post being published.")
+        ],
+        canonical_url: Annotated[
+            str, typer.Argument(help="The URL of the original blog post.")
+        ],
+        dry_run: Annotated[
+            bool, typer.Option(help="If true, then the draft posts will be generated and written to file only. Nothing is posted to dev.to or Medium.")
+        ] = False,
+        only_medium: Annotated[
+            bool, typer.Option(help="If true, then only Medium is posted to.")
+        ] = False,
+        only_dev: Annotated[
+            bool, typer.Option(help="If true, then only Dev.to is posted to.")
+        ] = False,
+):
+    """Publish draft blog posts on Dev.to and Medium of the specified markdown blog post file."""
+
+    if not validators.url(canonical_url):
+        raise Exception("The Canonical URL you provided is not valid.")
+    
+    if not blog_filepath.exists():
+        raise Exception("The filepath provided does not exist.")
+    
+    if only_dev and only_medium:
+        raise Exception("--only-dev and --only-medium cannot be called together.")
+    
+    post = BlogPostParser(blog_filepath.read_text())
+
+    if not only_dev:
+        post.send_to_medium(canonical_url, dry_run)
+
+    if not only_medium:
+        post.send_to_dev(canonical_url, dry_run)
+
+    if not dry_run:
+        print("REMEMBER: Copy and paste the version from [bold blue]Medium[/bold blue] into [bold yellow]LinkedIn[/bold yellow]")
